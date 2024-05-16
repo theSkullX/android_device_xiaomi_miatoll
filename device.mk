4b69da5f564efb3af4fea0a2aa7c14d9fe446d47
@cyberknight777 <cyberknight755@gmail.com>
2024-05-16 21:06:58 +0530

    miatoll: Drop telephony packages - These are built from vendor/yaap now.
    Change-Id: Id07740ad8fa0b0aa635e84d6e571c3a9c79c4975
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -430,19 +430,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi
 
-# Telephony
-PRODUCT_PACKAGES += \
-    ims-ext-common \
-    ims_ext_common.xml \
-    qti-telephony-hidl-wrapper \
-    qti_telephony_hidl_wrapper.xml \
-    qti-telephony-utils \
-    qti_telephony_utils.xml \
-    telephony-ext
-
-PRODUCT_BOOT_JARS += \
-    telephony-ext
-
+# Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal-service.pixel \
     thermal_symlinks
