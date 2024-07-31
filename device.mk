587fbf5fd8d8bf1a16316c97f664a4498fb9aa3b
@RealJohnGalt <7730016+RealJohnGalt@users.noreply.github.com>
2024-07-31 16:15:22 +0530

    miatoll: Import two vendor int files to dt For task_profiles migration

--- device.mk
+++ device.mk
@@ -407,6 +407,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
 
+# Vendor init
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/android.hardware.drm@1.3-service.widevine.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.drm@1.3-service.widevine.rc \
+    $(LOCAL_PATH)/configs/android.hardware.neuralnetworks@1.3-service-qti.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.neuralnetworks@1.3-service-qti.rc
+
 # Vendor service manager
 PRODUCT_PACKAGES += \
     vndservicemanager
