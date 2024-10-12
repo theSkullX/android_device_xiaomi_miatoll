b1fe6b64b27d8c9157872e9850fa8fa9cfe52db6
LuK1337 <priv.luk@gmail.com>
2024-10-12 17:42:02 +0200

    miatoll: Shim WFD with libgui_shim/libinput_shim
    Change-Id: I42bf06bc6b19942303031ef0147dafa6df127fe5

--- extract-files.sh
+++ extract-files.sh
@@ -86,6 +86,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             grep -q "libcamera_provider_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcamera_provider_shim.so" "${2}"
             ;;
+        system_ext/lib64/libwfdmmsrc_system.so)
+            [ "$2" = "" ] && return 0
+            grep -q "libgui_shim.so" "${2}" || "${PATCHELF}" --add-needed "libgui_shim.so" "${2}"
+            ;;
         system_ext/etc/init/wfdservice.rc)
             [ "$2" = "" ] && return 0
             sed -i "/^service/! s/wfdservice$/wfdservice64/g" "${2}"
@@ -94,6 +98,7 @@ function blob_fixup() {
         system_ext/lib64/libwfdnative.so)
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
+            grep -q "libinput_shim.so" "${2}" || "${PATCHELF}" --add-needed "libinput_shim.so" "${2}"
             ;;
         system_ext/lib64/libwfdservice.so)
             [ "$2" = "" ] && return 0
